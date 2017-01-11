75bca136377c1d981b369b10cb7d71f4d71315ea
Wayne Ellison <wcellison1@gmail.com>
2017-01-11 11:16:12 -0500

    initial commit js

---
+++ 0.01_interers.js
@@ -0,0 +1,14 @@
+//age
+console.log(44);
+//birth year
+console.log(1973);
+//grad yr
+console.log(1991);
+//numbers of bros
+console.log(1);
+
+console.log(10%3);
+console.log(12%5);
+console.log(142%13);
+console.log(10e2);
+console.log(Math.pow(10,2));
---
+++ 0.02_variables.js
@@ -0,0 +1,61 @@
+//this variable is a string type
+var firstname = "chris"
+var lastname =	"ellison"
+var fullname = firstname + " " + lastname
+//concatenation (this is where you can build or combine strings)
+console.log(firstname + " " + lastname);
+
+//empty variables
+var num = //has not been defined yet
+
+num = 2 // has been defined
+console.log(num);
+
+//empty string
+var petsName = "" //not defined string
+
+petsName = "tucker" // defined string
+
+console.log(petsName);
+
+//short hand operators
+var age = 44
+//console.log(1+=1);//sees this error as 1=1+1
+console.log(age += 1);
+//console.log(age /=2)
+console.log(age*=2);
+
+//var 2nite ="wednesday night"
+
+var int = 5
+
+//bronze challenge
+var numberOfPets = 2
+console.log(numberOfPets);
+
+var myCars = 4
+console.log(myCars);
+
+var myHomes = 16
+console.log(myHomes);
+
+var myCats = 1
+console.log(myCats);
+
+var numberOfBottles = 130
+console.log(numberOfBottles);
+
+//silver challenge
+var myNumber = 0
+
+console.log(myNumber+= 10);
+console.log(myNumber-=1);
+console.log(myNumber*=9);
+console.log(myNumber/=7);
+
+//gold chanllenge
+var age = 44
+var maxAge = 144
+var numberPerDay = 4
+var totalNeeded = (numberPerDay * 365) * (maxAge - age);
+console.log(totalNeeded);
