7388cdc3f575739d838b1738f33480925101c5d9
Wayne Ellison <wcellison1@gmail.com>
2017-01-17 20:45:03 -0500

    practice

---
+++ 0.031_strings.js
@@ -0,0 +1,15 @@
+var lastName ="ellison"
+var myDog = "tucker"
+var message = "Hi"+ " " + lastName + myDog
+var age = 2
+console.log(typeof message);
+console.log(message);
+console.log(typeof age);
+
+var greetings = 'the dog says "hi\'s"' 
+//bc of the 's needs \ bf 's
+console.log(greetings)
+
+console.log(greetings.split(""));
+
+console.log(greetings.slice(0,5));
