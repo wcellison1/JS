449de421f9bd672ba3f87fb13db9c0de088d1d7c
Wayne Ellison <wcellison1@gmail.com>
2017-02-23 07:48:37 -0500

    update

--- 0.0091_functions_method_invocation.js
+++ 0.0091_functions_method_invocation.js
@@ -1,1 +1,11 @@
+var myObject = {
+ value: 0,
+ increment: function (inc) {
+ this.value += typeof inc === 'number' ? inc : 1;
+ }
+};
+myObject.increment(2);
+console.log(myObject.value);
 
+myObject.increment(2);
+console.log(myObject.value);
